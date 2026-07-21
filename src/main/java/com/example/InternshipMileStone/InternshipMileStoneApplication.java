676da4ad2e2185466ab21b6650b52f9a4d9b85b4
Admin <ghaithzak9@gmail.com>
2026-07-21 14:14:56 +0300

    first commit

---
+++ src/main/java/com/example/InternshipMileStone/InternshipMileStoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.InternshipMileStone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipMileStoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipMileStoneApplication.class, args);
+	}
+
+}
